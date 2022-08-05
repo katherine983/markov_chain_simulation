049b6aa50caef58752389194559a3ddc6c9513ce
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-08-05 11:02:54 -0700

    moved gen_model to main.py

--- mc_measures/gen_mc_transition.py
+++ mc_measures/gen_mc_transition.py
@@ -491,33 +491,6 @@ class GenMarkovTransitionProb:
             MC = cls(text, data['MC_order'], transition_freq_matrix)
             return MC
 
-def gen_model(root_dir, order_i, states_temp):
-    """
-    Function to generate a MC matrix, calculate its entropy rate and save all
-    of its data to a json file.
-
-    Parameters
-    ----------
-    root_dir : PATH-LIKE OBJECT
-        THE ROOT DIRECTORY FOR THE OUTPUT FILE.
-    order_i : INTEGER
-        ORDER OF THE MARKOV MATRIX TO BE GENERATED.
-    states_temp : LIST OF UNICODE CHARACTERS
-        lIST OF THE UNICODE CHARACTERS THAT WILL BE THE STATES OF THE MARKOV
-        PROCESS.THE LENGTH OF states_temp SHOULD BE THE SAME AS THE SIZE OF THE
-        ALPHABET.
-
-    Returns
-    -------
-    MC_model : instance of GenMarkovTransitionProb.
-
-    """
-    MC_model = GenMarkovTransitionProb(states_temp, order_i)
-    MC_model.entropy_rate()
-    fout_file_path = create_output_file_path(root_dir, r'mc_matrices', f'Order{order_i}Alph{MC_model.m}ER{MC_model.ent_rate:.4f}.json')
-    MC_model.dump(fout_file_path)
-    return MC_model
-
 def get_model(file_path):
     mc_loaded = GenMarkovTransitionProb.load(file_path)
     return mc_loaded
---
+++ mc_measures/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  5 10:59:49 2022
+
+@author: Wuestney
+"""
+
