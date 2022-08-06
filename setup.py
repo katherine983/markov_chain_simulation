8c22c6202ffff4ae8e3ca21859c77805eb65fec0
katherine983 <53629686+katherine983@users.noreply.github.com>
2022-08-05 18:31:13 -0700

    update pkg version in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
       name = "mc_measures",
-      version = "0.1.",
+      version = "0.1.5",
       description = "A package for generating markov chain matrices to simulate markov chains and measure their entropy.",
       long_description = long_description,
       long_description_content_type = "text/markdown",
